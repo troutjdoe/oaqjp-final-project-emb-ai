8d939d875629ad589763e8ce516aee3152ceb53a
Mareks <you@example.com>
2024-09-15 10:34:35 -0400

    pylitn fixes

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
+"""
+Emotion Detection Web Application
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-
 app = Flask("Emotion Detector")
-
 @app.route("/emotionDetector")
 def sent_detector():
+    """
+    Route to analyze emotions in a given text.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-
     response = emotion_detector(text_to_analyze)
-
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
@@ -21,12 +23,12 @@ def sent_detector():
         f"'sadness': {response['sadness']}. "
         f"The dominant emotion is {response['dominant_emotion']}."
     )
-
     return output
-
 @app.route("/")
 def render_index_page():
+    """
+    Route to render the root /.
+    """
     return render_template('index.html')
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
